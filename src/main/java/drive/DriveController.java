c4843af0450cc6a6f4256de52567abccc1d1417b
KangJungu <KangJungu@JHJG.com>
2016-05-11 21:41:23 +0900

    1. pom.xml 에 json 라이브러리 4개추가
    <dependency>
    			<groupId>com.fasterxml.jackson.core</groupId>
    			<artifactId>jackson-core</artifactId>
    			<version>2.4.1</version>
    		</dependency>
    		<dependency>
    			<groupId>com.fasterxml.jackson.core</groupId>
    			<artifactId>jackson-databind</artifactId>
    			<version>2.4.1.1</version>
    		</dependency>
    		<dependency>
    			<groupId>org.codehaus.jackson</groupId>
    			<artifactId>jackson-core-asl</artifactId>
    			<version>1.9.13</version>
    		</dependency>
    		<dependency>
    			<groupId>org.codehaus.jackson</groupId>
    			<artifactId>jackson-mapper-asl</artifactId>
    			<version>1.9.13</version>
    		</dependency>
    
    2. CORSFilter java파일 만든 후 web.xml에 추가
     <filter>
        	<filter-name>CORSFilter</filter-name>
        	<filter-class>drive.CORSFilter</filter-class>
         </filter>
         <filter-mapping>
         	<filter-name>CORSFilter</filter-name>
         	<url-pattern>/*</url-pattern>
         </filter-mapping>
    
    3. dispatcher-servlet에 추가
    
    <mvc:annotation-driven
    		content-negotiation-manager="contentNegotiationManager" />
    	<bean id="contentNegotiationManager"
    		class="org.springframework.web.accept.ContentNegotiationManagerFactoryBean">
    		<property name="favorPathExtension" value="false" />
    		<property name="favorParameter" value="true" />
    		<property name="mediaTypes">
    			<value>
    				json=application/json
    				xml=application/xml
    			</value>
    		</property>
    	</bean>

--- src/main/java/drive/DriveController.java
+++ src/main/java/drive/DriveController.java
@@ -13,59 +13,68 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class DriveController {
-	@Autowired DriveMapper driveMapper;
-	@Autowired DriveService driveService;
+	@Autowired
+	DriveMapper driveMapper;
+	@Autowired
+	DriveService driveService;
 
-	@RequestMapping(value="/pdrive/main.pd" ,method = RequestMethod.GET)
-	public String main(@RequestParam("id2") int id2,Model model) {
+	@RequestMapping(value = "/pdrive/main.pd", method = RequestMethod.GET)
+	public String main(@RequestParam("id2") int id2, Model model) {
 		List<Drive> drive = driveMapper.selectDriveInfo(id2);
 		model.addAttribute("main", drive);
 		return "pdrive/main";
 	}
-	
-	@RequestMapping(value="/pdrive/folderList.pd" ,method = RequestMethod.GET)
-	public String folderlist(@RequestParam("dr_id") int dr_id,Model model) {
+
+	@RequestMapping("/pdrive/test.pd")
+	public @ResponseBody List<Drive> list() {
+		System.out.println("test");
+		return driveMapper.selectFolderAll();
+	}
+
+	@RequestMapping(value = "/pdrive/folderList.pd", method = RequestMethod.GET)
+	public String folderlist(@RequestParam("dr_id") int dr_id, Model model) {
 		List<Folder> dr1 = driveMapper.selectBydr_id1(dr_id);
 		List<Folder> dr2 = driveMapper.selectBydr_id2(dr_id);
 		model.addAttribute("dr1", dr1);
 		model.addAttribute("dr2", dr2);
 		return "pdrive/folderList";
 	}
-	
-	@RequestMapping(value="/pdrive/folderList2.pd" ,method = RequestMethod.GET)
-	public String folderlist2(@RequestParam("parent_id") int parent_id,Model model) {
+
+	@RequestMapping(value = "/pdrive/folderList2.pd", method = RequestMethod.GET)
+	public String folderlist2(@RequestParam("parent_id") int parent_id, Model model) {
 		List<Drive> pr = driveMapper.selectBypr_id(parent_id);
 		List<Drive> all = driveMapper.selectFolderAll();
 		model.addAttribute("pr", pr);
-		model.addAttribute("all",all);
+		model.addAttribute("all", all);
 		return "pdrive/folderList2";
 	}
-	
-	@RequestMapping(value="/pdrive/fileList.pd" ,method = RequestMethod.GET)
-	public String filelist(@RequestParam("fd_id") int fd_id,Model model) {
+
+	@RequestMapping(value = "/pdrive/fileList.pd", method = RequestMethod.GET)
+	public String filelist(@RequestParam("fd_id") int fd_id, Model model) {
 		List<Drive> fd = driveMapper.selectByf_id(fd_id);
 		model.addAttribute("fd", fd);
 		return "pdrive/fileList";
 	}
-	
-	@RequestMapping(value="/popup/createFolder.pd",method = RequestMethod.GET)
-	public String createFolder(@RequestParam("dr_id") Integer dr_id,Model model){
+
+	@RequestMapping(value = "/popup/createFolder.pd", method = RequestMethod.GET)
+	public String createFolder(@RequestParam("dr_id") Integer dr_id, Model model) {
 		Folder folder = driveMapper.selectBydr_id(dr_id);
 		model.addAttribute("folder", folder);
 		return "popup/createFolder";
 	}
-	
-	@RequestMapping(value="/popup/createFolder.pd",method = RequestMethod.POST)
-	public String createFolder(Folder folder,Model model) throws Exception {
+
+	@RequestMapping(value = "/popup/createFolder.pd", method = RequestMethod.POST)
+	public String createFolder(Folder folder, Model model) throws Exception {
 		String message = driveService.validatepw(folder);
-		if (message == null){
+		if (message == null) {
 			driveMapper.insert_sfolder(folder);
 			model.addAttribute("successMsg", "저장했습니다.");
-		}else{
+		} else {
 			driveMapper.insert_folder(folder);
 			model.addAttribute("successMsg", "저장했습니다.");
 		}
@@ -74,10 +83,10 @@ public class DriveController {
 
 	@RequestMapping(value = "/pdrive/fileUpload.pd", method = RequestMethod.POST)
 	public String upload(@RequestParam("files") MultipartFile uploadedFile) throws IOException {
-		if (uploadedFile.getSize() > 0 ) {
+		if (uploadedFile.getSize() > 0) {
 			Files files = new Files();
 			files.setFiles_name(Paths.get(uploadedFile.getOriginalFilename()).getFileName().toString());
-			files.setFiles_size((int)uploadedFile.getSize());
+			files.setFiles_size((int) uploadedFile.getSize());
 			files.setData(uploadedFile.getBytes());
 			files.setFolder_id(files.folder_id);
 			driveMapper.insert_files(files);
@@ -85,22 +94,19 @@ public class DriveController {
 		return "redirect:/pdrive/main.pd";
 	}
 	/**
-	@RequestMapping("/pdrive/fileDelete.pd")
-	public String delete(@RequestParam("id") int id) {
-		driveMapper.delete(id);
-		return "redirect:/pdrive/main.pd";
-	}
-
-	@RequestMapping("/pdrive/fileDownload.pd")
-	public void download(@RequestParam("fd_id") int fd_id, HttpServletResponse response) throws IOException {
-		List<Drive> drive = driveMapper.selectById(fd_id);
-		String f_name = URLEncoder.encode(((Drive) drive).getF_name(),"UTF-8");
-		response.setContentType("application/octet-stream");
-		response.setHeader("Content-Disposition", "attachment;f_name=" + f_name + ";");
-		try (BufferedOutputStream output = new BufferedOutputStream(response.getOutputStream())) {
-			output.write(((Drive) drive).getData());
-		}
-	}
-	**/
+	 * @RequestMapping("/pdrive/fileDelete.pd") public String
+	 * delete(@RequestParam("id") int id) { driveMapper.delete(id); return
+	 * "redirect:/pdrive/main.pd"; }
+	 * 
+	 * @RequestMapping("/pdrive/fileDownload.pd") public void
+	 * download(@RequestParam("fd_id") int fd_id, HttpServletResponse response)
+	 * throws IOException { List<Drive> drive = driveMapper.selectById(fd_id);
+	 * String f_name = URLEncoder.encode(((Drive) drive).getF_name(),"UTF-8");
+	 * response.setContentType("application/octet-stream");
+	 * response.setHeader("Content-Disposition", "attachment;f_name=" + f_name +
+	 * ";"); try (BufferedOutputStream output = new
+	 * BufferedOutputStream(response.getOutputStream())) { output.write(((Drive)
+	 * drive).getData()); } }
+	 **/
 
 }
